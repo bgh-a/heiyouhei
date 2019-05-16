7b16e685c67aef1febf0927227a0602a0dee42df
akbgh <1340112946@qq.com>
2019-05-17 00:42:29 +0800

    这是node回调函数

---
+++ 01-回调函数.js
@@ -0,0 +1,22 @@
+// 回调函数
+ function show(){
+
+    console.log("在北京我敢不努力吗？");
+ }
+
+
+
+ setInterval(show,1000);
+
+ /*
+ 
+    setInterval函数的定义：
+        function setInterval(fn,time){
+
+            // ...计时... (time)
+
+            fn();
+        }
+ 
+ 
+ */
